1a321983c2f61f0abefd53145e5183ce3925dd7b
Jesus Farias <jesus.fg2630@gmail.com>
2022-10-07 00:23:02 -0300

    Pusheo antes de arrancar con los Models

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require('express');
-const routes  = require('./routes')
+const expressEjsLayouts = require('express-ejs-layouts');
+const ejslayout = require('express-ejs-layouts')
+const routes  = require('./routes');
 require('dotenv').config();
 
 
 //Creo aplicación
 const app = express()
 
+//Ejs
+app.use(expressEjsLayouts);
+app.set('view engine', 'ejs');
 
-//Middleware
+//Estaticos
+app.use(express.static('public'))
 
-//Routes
+//Middlewares
+app.use((req,res,next) =>{
+    const fecha = new Date();
+    res.locals.year = fecha.getFullYear ();
+
+    next()
+})
+
+
+//Rutas
 app.use('/', routes());
-
 
 
 //Levanto servidor
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
+const homeControllers = require('../controllers/homeControllers')
+const usuariosControllers = require('../controllers/usuariosControllers')
+
 
 //Inicializo el router
 const router = express.Router()
 
 //Rutas
 module.exports = function(){
-    router.get('/', (req,res) =>{
-        res.send('HomePage')
-    })
+    router.get('/', homeControllers.home)
 
-    router.get('/crear-cuenta', (req,res) =>{
-        res.send('Crear cuenta')
-    })
+    router.get('/crear-cuenta', usuariosControllers.formCrearCuenta)
 
     return router;
 }
